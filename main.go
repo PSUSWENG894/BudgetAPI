b6483affa93afa0db12a08c93ca04c59232e6146
Steven Ford <evensteven01@gmail.com>
2018-06-13 22:04:01 -0400

    Added an endpoint with gin

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Printf("Hello world!")
+	msg := "Hello World!"
+	fmt.Printf(msg)
+
+	r := gin.Default()
+	r.GET("/", func(ctxt *gin.Context){
+		ctxt.JSON(200, gin.H{"message": msg},)
+	})
+	r.Run()
 }
