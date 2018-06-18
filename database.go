d1a12b51cda93640e44c3bcfc004ddc8577585bc
Steven Ford <evensteven01@gmail.com>
2018-06-17 21:25:24 -0400

    Fixed unused v

--- database.go
+++ database.go
@@ -12,4 +12,23 @@ func setupDatabase(){
 		os.Getenv("DATABASE_URL"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), 
 		os.Getenv("DB_PASSWORD"))
 	db, err := sql.Open("postgres", dbConnString)
+	if err != nil {
+		msg += ". Got error connecting to db: " + err.Error()
+		fmt.Printf(msg)
+	}
+	err = db.Ping()
+	if err != nil {
+		msg += ". Got error connecting to db: " + err.Error()
+		fmt.Printf(msg)
+	} else {
+		fmt.Printf("Pinged DB successfully: " + os.Getenv("DATABASE_URL"))
+	}
+
+	// createTables()
 }
+
+// funct createTables(){
+// 	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+//         fmt.Printf("Error creating database table: %q", err)
+//     }
+// }
